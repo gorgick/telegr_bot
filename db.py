096e31eb75ebd323a4cfdbd9742a654fef761c6d
gorgick <gorgick2014@yandex.by>
2026-03-05 20:18:06 +0300

    class Database

---
+++ db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    def user_exists(self, user):
+        with self.connection:
+            result = self.cursor.execute("select * from bot_owner where username == ?", (user,)).fetchmany(1)
+            return bool(len(result))
+
+    def add_user(self, user):
+        with self.connection:
+            return self.cursor.execute("insert into bot_owner (username) values (?)", (user,))
