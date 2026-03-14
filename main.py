bb961e8fdeb4657a9c8675d48174e15281128380
gorgick <gorgick2014@yandex.by>
2026-03-14 20:08:10 +0300

    markup

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ bot = Bot('8630054747:AAGCnVlkfExp6AopLEyBP4EuG-SNZJVINXk')
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands="start")
+async def cmd_start(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button_1 = types.KeyboardButton(text="Button 1")
+    button_2 = types.KeyboardButton(text="Button 2")
+    keyboard.add(button_1, button_2)
+    await message.answer("Choose an option:", reply_markup=keyboard)
+
+
 @dp.message_handler()
 async def info(message):
     if message.text.lower() == 'hello':
